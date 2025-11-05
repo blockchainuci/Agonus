32d1dd2917f94fd6484ee575f8945d83ed3d735c
SAMIKA BHAVESH BHATKAR <samikabhatkar@gmail.com>
2025-11-04 22:16:23 -0800

    Rough draft, added sections, will style

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
+import FeaturedAgents from './components/FeaturedAgents';
 import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
         <h1 className="text-8xl font-bold animate-pulse text-white">Agonus</h1>
         <p className="text-gray-300">Developed by Blockchain UCI</p>
         <Hero />
+        <HowItWorks />
+        <FeaturedAgents />
       </section>
     </main>
   );
