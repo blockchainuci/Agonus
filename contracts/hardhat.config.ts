ea401cac0d955c108bc9669be393ac05f83dabb9
SumanthPal <sumanth@pallamreddy.com>
2025-11-18 23:31:11 -0800

    added contract deployement code for base sepolia

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -36,6 +36,12 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    baseSepolia: {
+      type: "http",
+      chainType: "op",
+      url: configVariable("BASE_SEPOLIA_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+    },
   },
 };
 
